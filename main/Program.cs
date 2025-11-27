b81d38e238ee0b00a17538f2a9cae62174e17dc6
d0s1 <dominic1020@web.de>
2025-11-27 14:51:02 +0100

    added calculation

--- main/Program.cs
+++ main/Program.cs
@@ -1,7 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Enter Text");
+Random rnd = new Random();
+int firstNumber = rnd.Next(1,100);
+int secondNumber = rnd.Next(1,100);
+Console.WriteLine($"{firstNumber} + {secondNumber}");
 string input = Console.ReadLine();
+if (firstNumber + secondNumber ==  Int32.Parse(input)){
+    Console.WriteLine("Right!");
+}
+else{
+    Console.WriteLine($" Wrong! {firstNumber} + {secondNumber} = {firstNumber+secondNumber}");
+}
+input = Console.ReadLine();
 Console.WriteLine(input);
 var userid = 0;
 var Bog = new Bogus.Faker<User>()
